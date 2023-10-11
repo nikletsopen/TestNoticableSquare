44ba32d95a96987fe2a8fa6d58021f369bf02349
Nikita Timonin <nikita@letsopen.co>
2023-10-11 16:38:33 +0300

    Initial Commit

---
+++ NoticableSquare/NoticableSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoticableSquareApp.swift
+//  NoticableSquare
+//
+//  Created by Nikita Timonin on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NoticableSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
