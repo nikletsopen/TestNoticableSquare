e88aaf1662097019648cb607368b8c9264c4c6cf
Nikita Timonin <nikita@letsopen.co>
2023-10-12 17:47:12 +0300

    Solution

--- NoticableSquare/ContentView.swift
+++ NoticableSquare/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var position: CGSize = .zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 0) {
+                Color.white
+                Color.pink
+                Color.yellow
+                Color.black
+            }.ignoresSafeArea()
+            
+           Rectangle()
+                .fill(.white)
+                .blendMode(.difference)
+                .overlay(content: {
+                    Rectangle()
+                        .blendMode(.hue)
+                })
+                .overlay(content: {
+                    Rectangle()
+                        .fill(.white)
+                        .blendMode(.overlay)
+                })
+                .overlay(content: {
+                    Rectangle()
+                        .fill(.black)
+                        .blendMode(.overlay)
+                })
+                .frame(width: 90.0, height: 90.0)
+                .cornerRadius(10.0)
+                .offset(position)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            self.position = value.translation
+                        })
+                        .onEnded({ value in
+                            withAnimation {
+                                self.position = .zero
+                            }
+                        })
+                )
         }
-        .padding()
     }
 }
 
